2957049f404047a85513af9c93a1a0471e055249
Ubuntu <ubuntu@ip-172-31-43-137.ec2.internal>
2020-10-29 12:36:06 +0000

    validation changes

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,7 +1,7 @@
 class Contribution < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :url_or_text
-  
+
   def url_or_text
     ((url.blank? and text) or (url and text.blank?))
   end
