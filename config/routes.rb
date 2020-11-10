3222669901175ed1041e584f8adc9b86ee12534d
Ubuntu <ubuntu@ip-172-31-0-23.ec2.internal>
2020-11-10 10:35:33 +0000

    carpeta de views hechas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   
+  
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new', as: :new_user_session
+    get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  end
   
   get 'login', to: 'users#new'
   post 'login', to: 'users#create'
